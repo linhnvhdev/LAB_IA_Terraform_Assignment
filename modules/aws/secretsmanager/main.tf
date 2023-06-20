4ea8b235d2f36466ca49f1805f54b0d72c3f334a
Ngo Viet Hoang Linh <linhnvhdev@gmail.com>
2023-06-20 20:10:40 +0700

    Fix secret manager low risk and add output kms

--- modules/aws/secretsmanager/main.tf
+++ modules/aws/secretsmanager/main.tf
@@ -1,6 +1,7 @@
 resource "aws_secretsmanager_secret" "ec2_secret" {
   name = "ec2_secret"
   description = "To store password in user data in ec2"
+  kms_key_id = var.kms_key_id
 }
 
 resource "aws_secretsmanager_secret_version" "service_user" {
--- modules/aws/secretsmanager/variables.tf
+++ modules/aws/secretsmanager/variables.tf
@@ -2,3 +2,8 @@ variable "ec2_password" {
   description = "Password in ec2 userdata"
   type = string
 }
+
+variable "kms_key_id" {
+  description = "kms customer managed key id to encrypt"
+  type = string
+}
